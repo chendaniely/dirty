15d40198a16d8ee2cb7f490edb6f6bb5906717c9
Hugo Bowne-Anderson <hugo@datacamp.com>
2019-05-02 13:29:57 -0400

    Add function that removes data from file

--- prototype/remove_data.R
+++ prototype/remove_data.R
@@ -1,20 +1,14 @@
 # Load package(s)
 library(tidyverse)
 
-
-# Read in csv
-df <- readr::read_csv("data/mtcars.csv")
-
-# select 50% of rows & colummns
-rows <- sample(1:nrow(df), nrow(df)/2)
-cols <- sample(1:ncol(df), ncol(df)/2)
-
-# Replace values with empty string
-df[rows,cols] <- ''
-
-
-
-# output dirty csv
-readr::write_csv(df, "data/dirtycars.csv")
-
-
+remove_data <- function(input_file, output_file){
+  # Read in csv
+  df <- readr::read_csv(input_file)
+  # select 50% of rows & colummns
+  rows <- sample(1:nrow(df), nrow(df)/2)
+  cols <- sample(1:ncol(df), ncol(df)/2)
+  # Replace values with empty string
+  df[rows,cols] <- ''
+  # output dirty csv
+  readr::write_csv(df, output_file)
+}
